0fe4bcec99db8161c637eea6d9e151a964fb0d2b
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-07-28 15:51:22 +0200

    More CI 🧠

--- .github/actions/verify-chart-version/src/main.ts
+++ .github/actions/verify-chart-version/src/main.ts
@@ -2,7 +2,8 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as YAML from 'yaml';
 
-const fs = require('fs-extra');
+import * as semver from 'semver';
+import * as fs from 'fs-extra';
 
 function getErrorMessage(error: unknown) {
   if (error instanceof Error) return error.message;
@@ -24,14 +25,8 @@ async function run() {
       return;
     }
 
-    const updatedChartYamlContent = await fs.readFile(chartYamlPath, 'utf8');
-    const updatedChartYaml = await YAML.parse(updatedChartYamlContent);
-    if (!updatedChartYaml.version) {
-      core.setFailed(`${chartYamlPath} does not contain a version!`);
-      return;
-    }
-
     var originalChartYamlFile;
+    var originalChartVersion;
     const octokit = github.getOctokit(githubToken);
     try {
       originalChartYamlFile = await octokit.rest.repos.getContent({
@@ -46,16 +41,28 @@ async function run() {
 
     if (originalChartYamlFile && "content" in originalChartYamlFile.data){
       const originalChartYamlContent = Buffer.from(originalChartYamlFile.data.content, 'base64').toString('utf-8');
-      core.info(originalChartYamlContent);
       const originalChartYaml = await YAML.parse(originalChartYamlContent);
-      core.info(`Old chart version: ${originalChartYaml.version}`);
-
-      if (updatedChartYaml.version == originalChartYaml.version) {
-        core.info(`Chart version has not been updated!`)
-      }
+      originalChartVersion = originalChartYaml.version
     }
 
+    const updatedChartYamlContent = await fs.readFile(chartYamlPath, 'utf8');
+    const updatedChartYaml = await YAML.parse(updatedChartYamlContent);
+    if (!updatedChartYaml.version) {
+      core.setFailed(`${chartYamlPath} does not contain a version!`);
+      return;
+    }
+    const updatedChartVersion = updatedChartYaml.version;
+    if (!semver.valid(updatedChartVersion)) {
+      core.setFailed(`${updatedChartVersion} is not a valid SemVer version!`);
+      return;
+    }
+
+    core.info(`Old chart version: ${originalChartVersion}`);
     core.info(`New chart version: ${updatedChartYaml.version}`);
+
+    if (updatedChartYaml.version == originalChartVersion) {
+      core.setFailed(`Chart version has not been updated!`)
+    }
   }
   catch (error) {
     core.setFailed(getErrorMessage(error));
