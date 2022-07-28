cba0a6b816d1a48a85a90d6620eec5b99f7a6e3e
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-07-28 13:56:23 +0200

    More CI 🧠

---
+++ .github/actions/verify-chart-version-bump/lib/main.js
@@ -0,0 +1,57 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
+function getErrorMessage(error) {
+    if (error instanceof Error)
+        return error.message;
+    return String(error);
+}
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            if (github.context.eventName !== "pull_request") {
+                core.info("This function Can only run on pull requests!");
+                return;
+            }
+            const githubToken = core.getInput("token");
+            const chart = core.getInput('chart');
+        }
+        catch (error) {
+            core.setFailed(getErrorMessage(error));
+        }
+    });
+}
+run();
---
+++ .github/actions/verify-chart-version-bump/src/main.ts
@@ -0,0 +1,24 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+function getErrorMessage(error: unknown) {
+  if (error instanceof Error) return error.message
+  return String(error)
+}
+
+async function run() {
+  try {
+    if (github.context.eventName !== "pull_request") {
+      core.info("This function Can only run on pull requests!");
+      return;
+    }
+
+    const githubToken = core.getInput("token");
+    const chart = core.getInput('chart');
+  }
+  catch (error) {
+    core.setFailed(getErrorMessage(error));
+  }
+}
+
+run()
